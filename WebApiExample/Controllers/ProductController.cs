aab726ff607a9a5ad32b6a41e2168d204fcade41
kidfox2001 <35247418+kidfox2001@users.noreply.github.com>
2018-06-10 10:29:15 +0700

    10-6-2018

---
+++ WebApiExample/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiExample.Models;
+
+namespace WebApiExample.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private List<Product> products = new List<Product>()
+        {
+             new Product { id=1,name = "cup", price=22M, qty = 3 },
+             new Product { id=2,name = "paper", price=41.7M, qty = 3 }
+        };
+
+        public IEnumerable<Product> Get()
+        {
+            return products.ToList();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var p =  products.Where(q => q.id == id);
+            if (p == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(p);
+            }
+        }
+
+
+    }
+}
---
+++ WebApiExample/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiExample.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public int qty { get; set; }
+    }
+}
