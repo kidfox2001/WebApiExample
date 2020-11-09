5ae92e28b0be85961e5c8aed69c007d3c65a3717
kidfox2001 <35247418+kidfox2001@users.noreply.github.com>
2020-11-09 21:43:14 +0700

    9_11_2020_19_42

--- WebApiExample/Controllers/ProductController.cs
+++ WebApiExample/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Script.Serialization;
 using WebApiExample.Models;
 
 namespace WebApiExample.Controllers
@@ -17,10 +18,10 @@ namespace WebApiExample.Controllers
              new Product { id=3,name = "big", price=49.8M, qty = 1}
         };
 
-        public IEnumerable<Product> Get()
-        {
-            return products.ToList();
-        }
+        //public IEnumerable<Product> Get()
+        //{
+        //    return products.ToList();
+        //}
 
         public IHttpActionResult Get(int id)
         {
@@ -33,6 +34,29 @@ namespace WebApiExample.Controllers
             {
                 return Ok(p);
             }
+        }
+
+        public List<Student> Get()
+        {
+            var students = new List<Student>
+            {
+            new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+            new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            new Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
+            new Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            new Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
+            new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
+            new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+            };
+
+            students[0].Enrollments.Add(new Enrollment() { Grade = Grade.A, Course = new Course { CourseID = 1, Title = "33" } });
+
+            var j = Json(students);
+            string json = new JavaScriptSerializer().Serialize(j.Content);
+            var objs = new JavaScriptSerializer().Deserialize<List<Student>>(json);
+
+            return students;
         }
 
 
